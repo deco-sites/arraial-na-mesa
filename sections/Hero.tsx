12d987b6a2a8308114c6e9035835e8f4514e6a3f
Karla Oshikawa <karla_hara@hotmail.com>
2024-06-14 22:23:14 +0900

    feat: id hero

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -46,41 +46,49 @@ export default function HeroFlats({
             <a href="#1" class="hero--a ">
               <span class="hero--span ">doces</span>
             </a>
-            <img
-              class="hero--img"
-              src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/15c591de-8dcc-41d9-9495-a731d71ea528"
-              alt="doces de festa junina"
-            />
+            <a href="#1">
+              <img
+                class="hero--img"
+                src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/15c591de-8dcc-41d9-9495-a731d71ea528"
+                alt="doces de festa junina"
+              />
+            </a>
           </li>
           <li class="panel border-r-4 border-secondary">
             <a href="#2" class="hero--a">
               <span class="hero--span">salgados</span>
             </a>
-            <img
-              class="hero--img"
-              src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/e73261f2-87e4-4f6f-8560-d038970d0b2f"
-              alt="salgados de festa junina"
-            />
+            <a href="#2">
+              <img
+                class="hero--img"
+                src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/e73261f2-87e4-4f6f-8560-d038970d0b2f"
+                alt="salgados de festa junina"
+              />
+            </a>
           </li>
           <li class="panel border-r-4 border-secondary">
             <a href="#3" class="hero--a">
               <span class="hero--span">Bebidas</span>
             </a>
-            <img
-              class="hero--img"
-              src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/fc16cb01-e492-4973-b081-df2052257818"
-              alt="bebidas de festa junina"
-            />
+            <a href="#3">
+              <img
+                class="hero--img"
+                src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/fc16cb01-e492-4973-b081-df2052257818"
+                alt="bebidas de festa junina"
+              />
+            </a>
           </li>
           <li class="panel">
             <a href="#4" class="hero--a">
               <span class="hero--span">DIY</span>
             </a>
-            <img
-              class="hero--img"
-              src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/eb6afeb6-fcbc-4f62-9be3-6f1182b3f837"
-              alt="decoracao de festa junina"
-            />
+            <a href="#4">
+              <img
+                class="hero--img"
+                src="https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/10767/eb6afeb6-fcbc-4f62-9be3-6f1182b3f837"
+                alt="decoracao de festa junina"
+              />
+            </a>
           </li>
         </ul>
       </div>
